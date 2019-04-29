9d11f4952d35282013405c308f03f52fc23bc2bb
spaceTab <93plaistj@protonmail.ch>
2019-04-29 16:08:27 -0400

    changes

--- port/src/Pages/Landing/landing.js
+++ port/src/Pages/Landing/landing.js
@@ -3,24 +3,21 @@ import "./landing.css";
 // import Container from '../../Components/Container';
 //import Nav from '../../Components/Navbar';
 
+
+
 class Landing extends React.Component {
+
+  state = {
+    words:['Linux', 'UX/UI', 'Cats']
+  }
+
   render() {
     return (
       <>
         <div className="container">
           <div className="content">
             <h1 className="heading"> Jake Plaisted </h1>
-            <ul>
-              <li>US Based Web Developer </li>
-              <li>Linux Enthusiast</li>
-              <li>With a focus on modern designs, with friendly & clean UX's</li>
-            </ul>
-          </div>
 
-          <div className="btnContainer">
-            <input type="button" value="Button" className="btn draw-border">
-            
-            </input>
           </div>
         </div>
       </>
